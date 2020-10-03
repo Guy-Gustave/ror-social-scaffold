8a4b3ad50f3708cb86aef923bb5fe4fdb5cbb34c
Guy <guymoustapha@gmail.com>
2020-10-03 12:43:36 +0200

    fix and setup linters

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,10 +1,11 @@
 class FriendshipsController < ApplicationController
-  @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-  if @friendship.save
-    flash[:notice] = "Added friend."
-    redirect_to root_url
-  else
-    flash[:notice] = "Unable to add friend."
-    redirect_to root_url
+  def create
+    @friendship = current_user.friendships.new(friend_id: params[:user_id], status: false)
+
+    if @friendship.save
+      redirect_to users_path, notice: 'You sent a new invite.'
+    else
+      redirect_to users_path, alert: 'woohps something went wrong with the invite.'
+    end
   end
 end
