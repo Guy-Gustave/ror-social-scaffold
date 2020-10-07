c598b4256bf9dcf6269e15b91cc4b0a5eb1e741b
Guy <guymoustapha@gmail.com>
2020-10-08 00:23:34 +0200

    add some test as suggested by tse and change friendship model and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,10 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+
   has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
   has_many :friends, through: :confirmed_friendships
 
@@ -35,6 +37,10 @@ class User < ApplicationRecord
     friendship = inverse_friendships.find { |f| f.user == user }
     friendship.confirmed = true
     friendship.save
+  end
+
+  def friends_and_own_posts
+    Post.where(user_id: friends_ids)
   end
 
   def reject_request(user)
